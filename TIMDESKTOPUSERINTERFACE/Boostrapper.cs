64fb8a384d340fde01252d7e36eea2827400c31f
mahamoud02 <maj.jama90@gmail.com>
2020-09-09 14:17:54 +0400

    Add MVVM (shellviewmodel and shellview (window)) and  add Bootstrapper and setting in app.xaml

---
+++ TIMDESKTOPUSERINTERFACE/Boostrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TIMDESKTOPUSERINTERFACE.ViewModels;
+
+namespace TIMDESKTOPUSERINTERFACE
+{
+    public class Boostrapper : BootstrapperBase
+    {
+        public Boostrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+
+        }
+    }
+}
