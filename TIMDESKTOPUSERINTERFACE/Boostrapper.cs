498b64339f79a14f8feab892497d5300233758fa
mahamoud02 <maj.jama90@gmail.com>
2020-09-17 19:16:22 +0400

    create login view and login.cs and add passwordhelper

--- TIMDESKTOPUSERINTERFACE/Boostrapper.cs
+++ TIMDESKTOPUSERINTERFACE/Boostrapper.cs
@@ -5,20 +5,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using TIMDESKTOPUSERINTERFACE.Helper;
 using TIMDESKTOPUSERINTERFACE.ViewModels;
 
 namespace TIMDESKTOPUSERINTERFACE
 {
     public class Boostrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public Boostrapper()
         {
             Initialize();
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
+        }
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            _container.Singleton<IWindowManager, WindowManager>().
+                Singleton<IEventAggregator, EventAggregator>();
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(ViewModelsType => _container.RegisterPerRequest(ViewModelsType, ViewModelsType.ToString(), ViewModelsType));
+         //    _container.PerRequest<ICALCULATION, CALCULATION>();
         }
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
 
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
--- TIMDESKTOPUSERINTERFACE/ViewModels/ShellViewModel.cs
+++ TIMDESKTOPUSERINTERFACE/ViewModels/ShellViewModel.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using Caliburn.Micro;
 
 namespace TIMDESKTOPUSERINTERFACE.ViewModels
+
 {
-    public class ShellViewModel
+    public class ShellViewModel  :Conductor<object>
     {
+        private LoginViewModel _loginViewModel;
+
+        public ShellViewModel (LoginViewModel lginVM)
+        {
+            _loginViewModel = lginVM;
+            ActivateItem(_loginViewModel);
+            
+            
+                
+
+        }
+
+     
 
     }
+    //public Loginscreen()
+    //{
+
+    //}
+
 }
